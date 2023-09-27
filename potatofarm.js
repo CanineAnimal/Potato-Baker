f7f30dd41ef31f578584a333da03b4a73d0c693c
CanineAnimal <111728906+CanineAnimal@users.noreply.github.com>
2023-09-27 21:04:44 +1300

    Update potatofarm.js

--- potatofarm.js
+++ potatofarm.js
@@ -116,9 +116,11 @@ var func3 = function(){
   alert('Posted to TNP Gameside Voting Box. Preparing post to main TNP RMB...\n\nDo not close this tab.')
   
   // Amend post content for posting to TNP RMB itself
-  bbCode2 = 'Hey all! What do you think about the proposal [url=' + rezzy + ']' + rezzyXml.querySelector('NAME').innerHTML + '[/url] coming up soon ([url=' + dt + `]debate thread here[/url])?
+  bbCode2 = 'Hey all! What do you think about the WA proposal [url=' + rezzy + ']' + rezzyXml.querySelector('NAME').innerHTML + '[/url] coming up soon ([url=' + dt + `]debate thread here[/url])?
 
-If you're a WA member, you can have your vote on the resolution counted by following the instructions on the RMB of [region]TNP Gameside Voting Box[/region]. If you're not in the WA, feel free to debate the proposal on that RMB! :D`
+If you're a WA member, you can have your vote on the resolution counted by following the instructions on the RMB of [region]TNP Gameside Voting Box[/region]. If you're not in the WA, feel free to debate the proposal on that RMB! :D
+
+Do note that for your vote to be counted you'd have to follow the instructions on the RMB of [region]TNP Gameside Voting Box -- likewise I think it's best for debate to also go there, so that other voters can see it easier.`
   
   // Generate post to TNP RMB
   document.body.innerHTML = 'Copy the content in the textbox below into TNP\'s RMB (link: <A HREF="https://www.nationstates.net/page=display_region_rmb/region=the_north_pacific">here</A>). <STRONG>Once you have done so, feel free to close this tab.</STRONG><BR/><BR/><TEXTAREA>' + bbCode2.replaceAll('', '');
